5fb796a2cac3daed3dd398ad3cecc2c1dec0d487
Jean-LouisB <fkopf@orange.fr>
2025-01-25 12:38:13 +0100

    set

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ pip freeze > ./dep/requirements.txt
 
 touch .env
 mkdir tests
-touch /tests/__init__.py
-mkdir /logs_gunicorn
-touch /logs_gunicorn/access.log
-touch /logs_gunicorn/erreur.log
-git remote remove origin
+touch ./tests/__init__.py
+mkdir logs_gunicorn
+touch ./logs_gunicorn/access.log
+touch ./logs_gunicorn/erreur.log
+#git remote remove origin
 echo "Initialisation terminée. Activez l'environnement avec : source venv/bin/activate"
